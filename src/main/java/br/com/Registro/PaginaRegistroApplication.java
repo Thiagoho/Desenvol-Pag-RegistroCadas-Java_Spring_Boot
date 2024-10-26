0c7c79d40191a01de072c69332106b9736921653
Thiago H O S <thiago2006_sales@hotmail.com>
2024-10-26 17:38:01 -0300

    Desev o ambiente do project

---
+++ src/main/java/br/com/Registro/PaginaRegistroApplication.java
@@ -0,0 +1,13 @@
+package br.com.Registro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaRegistroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaRegistroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/Registro/PaginaRegistroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Registro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaRegistroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
